08e4e8fd8d21bd802cffa11842bd2ea47bcef511
César Carbajo <cesares10@gmail.com>
2019-10-10 12:11:48 +0200

    Fondos añadidos a la escena y personajes recolocados (WIP)
    Se ha intentado meter las nuevas animaciones del enemigo 3 pero produce un slowdown muy grande, estoy a la espera de una version menos pesada. Las posiciones de los personajes dan problemas con el resize, tengo que revisarlo luego

--- DOM Social Network/Scripts/preload.js
+++ DOM Social Network/Scripts/preload.js
@@ -98,10 +98,6 @@ var myPreload = {
       width: 1920,
       height: 1440
     },
-    //hero: {
-    //src: 'Assets/hero.jpg',
-    //width: 366,
-    //height: 688},
     enemy0: {
       src: 'Assets/spritesheet_test.png',
       width: 366,
@@ -153,47 +149,47 @@ var myPreload = {
       height: 688
     },
     room0: {
-      src: 'Assets/fondo1.jpg',
+      src: 'Assets/fondo1.png',
       width: 1920,
       height: 1440
     },
     room1: {
-      src: 'Assets/fondo2.jpg',
+      src: 'Assets/fondo2.png',
       width: 1920,
       height: 1440
     },
     room2: {
-      src: 'Assets/fondo3.jpg',
+      src: 'Assets/fondo3.png',
       width: 1920,
       height: 1440
     },
     room3: {
-      src: 'Assets/fondo4.jpg',
+      src: 'Assets/fondo4.png',
       width: 1920,
       height: 1440
     },
     room4: {
-      src: 'Assets/fondo5.jpg',
+      src: 'Assets/fondo5.png',
       width: 1920,
       height: 1440
     },
     room5: {
-      src: 'Assets/fondo6.jpg',
+      src: 'Assets/fondo6.png',
       width: 1920,
       height: 1440
     },
     room6: {
-      src: 'Assets/fondo7.jpg',
+      src: 'Assets/fondo7.png',
       width: 1920,
       height: 1440
     },
     room7: {
-      src: 'Assets/fondo8.jpg',
+      src: 'Assets/fondo8.png',
       width: 1920,
       height: 1440
     },
     room8: {
-      src: 'Assets/fondo9.jpg',
+      src: 'Assets/fondo9.png',
       width: 1920,
       height: 1440
     },
